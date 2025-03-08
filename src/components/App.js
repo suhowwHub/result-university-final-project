f2c83fcc7293f24402f9fe681413bf021a8697cb
Serafim Suhov <serafimsuhov@gmail.com>
2025-03-08 11:26:45 +0300

    Add basic markup for App.js and index.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,27 @@ import { List } from './List';
 import { ListItem } from './ListItem';
 
 export class App extends Component {
+
   setup(props) {
+    
+    this.state = {
+      total: 0,
+      donates: []
+    }
+
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'app';
 
-    // ...
+    const $amountTitle = document.createElement('h1')
+    const $amountWrapper = document.createElement('span')
+
+    $amountWrapper.textContent = this.state.total
+    
+    $amountTitle.className = 'total-amount'
+    $amountTitle.textContent = 'Итого: $'
+    $amountTitle.appendChild($amountWrapper)
+
+    this.$rootElement.appendChild($amountTitle)
     
     const donateForm = new Form();
     this.$rootElement.appendChild(donateForm.$rootElement);
@@ -20,3 +36,4 @@ export class App extends Component {
     // ...
   }
 }
+
